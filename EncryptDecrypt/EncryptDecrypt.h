5b8fb68b2e4793b0bd570c08456bb098b1aaa0cd
Ossi Heino <ossi.heino@gmail.com>
2019-02-13 15:35:55 +0200

    Another encryption

--- EncryptDecrypt/EncryptDecrypt.h
+++ EncryptDecrypt/EncryptDecrypt.h
@@ -6,9 +6,11 @@ class EncryptDecrypt
 {
 public:
 
+	// Encryption with given key
 	std::string Encrypt(std::string text, char key) {
 		std::string Encrypted = text;
 		// No encryption key given -> return
+		// TODO: Maybe when encryption key is not given, we should use the private key?
 		if (!key) return 0;
 
 		for (int i = 0; i < text.size(); i++)
@@ -32,6 +34,33 @@ public:
 
 		return Encrypted;
 	}
+
+	// Encryption with private key
+	std::string Encrypt(std::string text) {
+		std::string output = text;
+
+		for (int i = 0; i < text.size(); i++) {
+			output[i] = text[i] ^ EncryptKey[i % (sizeof(EncryptKey) / sizeof(char))];
+		}
+		return output;
+	}
+
+	std::string Decrypt(std::string text) {
+		std::string output = text;
+
+		for (int i = 0; i < text.size(); i++) {
+			output[i] = text[i] ^ EncryptKey[i % (sizeof(EncryptKey) / sizeof(char))];
+
+		}
+
+		return output;
+
+	}
+private:
+	// Encryption key, the more letters the better
+	// Can be any letters
+	char EncryptKey[5] = { 'E', 'K', 'E', 'E', 'Y' };
+
 
 };
 
